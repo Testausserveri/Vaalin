c808212f6fab499c975e2998ef85bcd05bba5304
PixelSergey <sergey.ichtchenko@gmail.com>
2025-03-18 22:02:23 +0000

    Minimise useEffect overhead

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -5,18 +5,12 @@ import { useTranslation } from "react-i18next"
 import { useEffect, useState } from "react"
 
 export default function Home() {
-  const { t, i18n } = useTranslation()
+  const { t } = useTranslation()
   const [ isClient, setIsClient ] = useState(false)
 
   useEffect(() => {
     setIsClient(true)
-
-    const match = document.cookie.match(new RegExp(`(^| )i18next=([^;]+)`))
-    const storedLang = match ? match[2] : null
-    if (storedLang && i18n.language !== storedLang) {
-      i18n.changeLanguage(storedLang)
-    }
-  }, [i18n])
+  })
 
   const heroProps = isClient
     ? {
